36241f956c4b50ca5b1e11009cb586660dad4b34
Felix <noname242510@gmail.com>
2025-10-03 10:00:56 +0000

    Try fixing this error: `Runtime Error: Error: DialogTrigger is not defin

--- src/app/dashboard/subjects/[subjectId]/_components/stack-item.tsx
+++ src/app/dashboard/subjects/[subjectId]/_components/stack-item.tsx
@@ -29,6 +29,7 @@ import {
   DialogHeader,
   DialogTitle,
   DialogFooter,
+  DialogTrigger,
 } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
