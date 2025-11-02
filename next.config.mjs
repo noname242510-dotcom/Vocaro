75d521933b1113927b10d5583d5bde673215c761
Felix <noname242510@gmail.com>
2025-11-02 19:06:27 +0000

    Passkey-Anmeldung fehlgeschlagen The 'publickey-credentials-get' feature

---
+++ next.config.mjs
@@ -0,0 +1,46 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Permissions-Policy',
+            value: 'publickey-credentials-get=*, publickey-credentials-create=*',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
