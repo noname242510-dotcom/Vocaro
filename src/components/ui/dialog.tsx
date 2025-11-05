d787e78691eb5ee74eeec2e4eef586596d9e8ec7
Felix <noname242510@gmail.com>
2025-11-05 13:38:32 +0000

    lasse mehr weißraum und platz zum x in der mobilen ansicht (_for element

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -59,7 +59,7 @@ const DialogHeader = ({
 }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
     className={cn(
-      "flex flex-col space-y-1.5 text-center sm:text-left",
+      "flex flex-col space-y-1.5 text-center sm:text-left sm:pt-0 pt-4",
       className
     )}
     {...props}
